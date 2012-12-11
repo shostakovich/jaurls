eac69386bb1830a3ea3308338cccf202d6c67fc4
Robert Curth <robert@rocu.de>
2012-12-11 19:39:56 +0100

    Fixes broken index.php

--- web/index.php
+++ web/index.php
@@ -3,14 +3,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-  'db.options' => array(
-    'driver'   => 'pdo_sqlite',
-    'path'     => __DIR__.'/app.db',
-  ),
-));
-
-$app->get('/{short_path}', function ($short_path) use ($app) {
+$app->get('/{short_path}', function ($short_path){
   return $short_path;
 });
 
